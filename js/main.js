c1f87af8a8f4c72479bca5a01734c9f8a740480f
ElJoho <johalopezari@unal.edu.co>
2025-07-12 23:47:10 -0500

    Se arreglo la sección de nosotros, se arreglaron algunos enlaces del menu.

--- js/main.js
+++ js/main.js
@@ -10,10 +10,44 @@ window.onload = function () {
     const hrefMap = {
         'The Rolling Volts':'index.html',
         'Inicio': 'index.html',
-        'Gestión de Proyecto': 'aprendizaje/gestion.html',
-        'Trabajo Colaborativo': 'aprendizaje/colaborativo.html',
-        'Proceso de Aprendizaje': 'aprendizaje/proceso.html',
-        'Recomendaciones': 'aprendizaje/recomendaciones.html',
+        'Proceso de Aprendizaje':'procesoDeAprendizaje/index.html', //
+        'Gestión de Proyecto':'procesoDeAprendizaje/index.html',
+        'Trabajo Colaborativo':'procesoDeAprendizaje/index.html',
+        'Proceso de Aprendizaje':'procesoDeAprendizaje/index.html',
+        'Recomendaciones':'procesoDeAprendizaje/index.html',
+        'Producto de Ingeniería':'productoDeIngenieria/index.html', //
+        'Modulos':'modulos/index.html', //
+        'Gestión de Producción y Automatización':'modulos/gestion/index.html', // //
+        'Análisis de simulación de planta':'modulos/gestion/index.html',
+        'Consideración de aspectos':'modulos/gestion/index.html',
+        'Propuesta para Sistema MES':'modulos/gestion/index.html',
+        'Industria 4.0 en la Automatización':'modulos/industria4/index.html', // //
+        'Diagramas de instrumentación':'modulos/industria4/index.html',
+        'Arquitectura de control':'modulos/industria4/index.html',
+        'Aplicación de Industria 4.0':'modulos/industria4/index.html',
+        'Planeación del Proyecto':'modulos/planeacion/index.html', // //
+        'Cronograma':'modulos/planeacion/index.html',
+        'Evaluación Económica de Proyectos':'modulos/evaluacionEconomica/index.html', // //
+        'Presupuesto Financiero':'modulos/evaluacionEconomica/index.html',
+        'Indicadores Financieros':'modulos/evaluacionEconomica/index.html',
+        'Oferta Comercial':'modulos/evaluacionEconomica/index.html',
+        'Celda de Manufactura Robotizada':'modulos/celdaManufactura/index.html', // //
+        'Análisis de Robotización':'modulos/celdaManufactura/index.html',
+        'Diseño de celda robotizada':'modulos/celdaManufactura/index.html',
+        'Celda en RobotStudio':'modulos/celdaManufactura/index.html',
+        'Análisis y Gestión de Riesgos':'modulos/celdaManufactura/index.html',
+        'Digital Factory':'modulos/digitalFactory/index.html', // //
+        'Modelación en Siemens NX':'modulos/digitalFactory/index.html',
+        'Controladores Industriales(PLC)':'modulos/PLC/index.html', // //
+        'Desglose de Problema':'modulos/PLC/index.html',
+        'Implementación':'modulos/PLC/index.html',
+        'SCADA':'modulos/SCADA/index.html', // //
+        'Desarrollo HMI':'modulos/SCADA/index.html',
+        'Incorporación HMI':'modulos/SCADA/index.html',
+        'Productos':'productos/index.html', //
+        'Scooter':'productos/index.html',
+        'Cargo E- Bike':'productos/index.html',
+        'Mountain E-Bike':'productos/index.html',
         'Nosotros': 'Nosotros/index.html',
     };
     const menu = new WebComponent(path, document.createElement('nav'), hrefMap, 'navbar sticky', 'menu.html', '.menu-item,.logo', 'first');
@@ -23,7 +57,7 @@ window.onload = function () {
     const favicon = new ScriptTag('link','',`href;${path}img/simbolos/logoWhite.svg`,'type;image/svg+xml','async;true','rel;icon');
     const footerCSS = new ScriptTag('link','',`href;${path}css/footer.css`,'rel;stylesheet preload prefetch','as;style','async;true');
     const scriptJS = new ScriptTag('script', '', `src;${path}js/script.js`, 'as;script', 'async;true');
-    const headerHomeJS = new ScriptTag('script','',`src;${path}js/headerHome.js`);
+    //const headerHomeJS = new ScriptTag('script','',`src;${path}js/headerHome.js`);
 
     //Usar esto para acceder a las rutas en vez de data-path en cada pagina
     const currentURL = window.location.href;
