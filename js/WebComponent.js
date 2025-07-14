cb68446d29e3316cda315f05bd597332f6311263
ElJoho <johalopezari@unal.edu.co>
2025-07-13 22:20:56 -0500

    Se pusieron de los cards modulos como webcomponents, aun hay que hacerlo un script y ponerlo en el index de la pagina inicial y tambien en el index de la carpeta modulos

--- js/WebComponent.js
+++ js/WebComponent.js
@@ -1,42 +1,62 @@
+
 import { htmlPath } from './htmlPath.js';
 import { Element } from './Element.js';
-export class WebComponent  extends Element{ 
-	constructor(path, element, hrefMap, className, htmlFile, hrefClass, position = 'last', options = {}) {
+
+export class WebComponent extends Element {
+    constructor(path, element, urlMap, className, htmlFile, hrefClass, position = 'last', options = {}) {
         super(element);
         this.path = path;//path es la ruta actual del archivo
         this.htmlFile = htmlFile;//htmlFile es un strind del nombre del .html que tiene el webComponent
         this.className = className; //className es un string con los nombres de la clase del presente elemento
-		this.hrefMap = hrefMap; // Son las rutas relativas a reemplazar
+		this.urlMap = urlMap; // Son las rutas relativas a reemplazar
 		this.hrefClass = hrefClass; //son las clases con las que luego se reemplaza href
 		this.position = position; //es si se añade primero o ultimo al body
 		//options permite cargar varias funciones a la clase WebComponent
 		this.onload = options.onload || null; //una función para cargar scripts u otros elementos despues de que se ha cargado el webcomponent
 		this.setAttributes(`class;${className}`);
 		this.load = this.load.bind(this);
-		if(!(this.htmlFile == undefined || this.hrefMap == undefined || this.hrefClass == undefined))
-			this.load();
 
-		this.addToElement(undefined,this.position);// cuando el primer parametro es indefinido. Este elemento se añade por defecto al body
-	}
+        this.addToElement(undefined, this.position);// cuando el primer parametro es indefinido. Este elemento se añade por defecto al body
 
-	load() {
-		fetch(`${this.path}webComponents/${this.htmlFile}`)
-			.then(res => res.text())
-			.then(data => { 
-    			//console.log("Contenido del footer cargado:", data);
-				this.setAttributes(`innerHTML;${data}`)
-				//this.element.innerHTML = data;
-				const base = htmlPath();//Después de insertar el elemento, actualiza los href
-				document.querySelectorAll(this.hrefClass).forEach(link => {
-					const text = link.textContent.trim();
-					if (this.hrefMap[text]) {
-						link.setAttribute('href', base + this.hrefMap[text]);
-					}
-				});
-				
-				if (typeof this.onload === 'function')this.onload(); // Onload va a cargar elementos depues de que el webcomponent se haya cargado
-			});
-			
+        if (!(this.htmlFile == undefined || this.urlMap == undefined || this.hrefClass == undefined)) {
+            this.load();
+        }
+    }
+
+    load() {
+        fetch(`${this.path}webComponents/${this.htmlFile}`)
+            .then(res => res.text())
+            .then(data => {
+                this.element.innerHTML = data;
+
+                const base = htmlPath();//Después de insertar el elemento, actualiza los href
+                const map = this.urlMap;
+
+                // Unificar todos los elementos con atributos tipo URL
+                const elementsWithSrc = [
+                    { selector: 'a', attr: 'href' },
+                    { selector: 'img', attr: 'src' },
+                    { selector: 'iframe', attr: 'src' },
+                    { selector: 'video', attr: 'src' },
+                    { selector: 'video', attr: 'poster' },
+                    { selector: 'audio', attr: 'src' },
+                    { selector: 'source', attr: 'src' },
+                    { selector: 'script', attr: 'src' },
+                    { selector: 'link', attr: 'href' }
+                ];
+
+                elementsWithSrc.forEach(({ selector, attr }) => {
+                    document.querySelectorAll(selector).forEach(el => {
+                        const val = el.getAttribute(attr);
+                        //console.log(base + map[val]);
+                        if (val && map[val]) {
+                            el.setAttribute(attr, base + map[val]);
+                        }
+                    });
+                });
+
+                if (typeof this.onload === 'function') this.onload();// Onload va a cargar elementos depues de que el webcomponent se haya cargado
+            })
+            .catch(err => console.error("Error loading WebComponent:", err));
     }
 }
-
--- js/main.js
+++ js/main.js
@@ -4,64 +4,50 @@ import { htmlPath } from "./htmlPath.js";
 import { WebComponent } from "./WebComponent.js";
 
 window.onload = function () {
-    //const HTML = new Element(document.getElementsByTagName('html')[0]);
     const path = htmlPath();
     console.log(path);
-    const hrefMap = {
-        'The Rolling Volts':'index.html',
-        'Inicio': 'index.html',
-        'Proceso de Aprendizaje':'procesoDeAprendizaje/index.html', //
-        'Gestión de Proyecto':'procesoDeAprendizaje/index.html',
-        'Trabajo Colaborativo':'procesoDeAprendizaje/index.html',
-        'Proceso de Aprendizaje':'procesoDeAprendizaje/index.html',
-        'Recomendaciones':'procesoDeAprendizaje/index.html',
-        'Producto de Ingeniería':'productoDeIngenieria/index.html', //
-        'Modulos':'modulos/index.html', //
-        'Gestión de Producción y Automatización':'modulos/gestion/index.html', // //
-        'Análisis de simulación de planta':'modulos/gestion/index.html',
-        'Consideración de aspectos':'modulos/gestion/index.html',
-        'Propuesta para Sistema MES':'modulos/gestion/index.html',
-        'Industria 4.0 en la Automatización':'modulos/industria4/index.html', // //
-        'Diagramas de instrumentación':'modulos/industria4/index.html',
-        'Arquitectura de control':'modulos/industria4/index.html',
-        'Aplicación de Industria 4.0':'modulos/industria4/index.html',
-        'Planeación del Proyecto':'modulos/planeacion/index.html', // //
-        'Cronograma':'modulos/planeacion/index.html',
-        'Evaluación Económica de Proyectos':'modulos/evaluacionEconomica/index.html', // //
-        'Presupuesto Financiero':'modulos/evaluacionEconomica/index.html',
-        'Indicadores Financieros':'modulos/evaluacionEconomica/index.html',
-        'Oferta Comercial':'modulos/evaluacionEconomica/index.html',
-        'Celda de Manufactura Robotizada':'modulos/celdaManufactura/index.html', // //
-        'Análisis de Robotización':'modulos/celdaManufactura/index.html',
-        'Diseño de celda robotizada':'modulos/celdaManufactura/index.html',
-        'Celda en RobotStudio':'modulos/celdaManufactura/index.html',
-        'Análisis y Gestión de Riesgos':'modulos/celdaManufactura/index.html',
-        'Digital Factory':'modulos/digitalFactory/index.html', // //
-        'Modelación en Siemens NX':'modulos/digitalFactory/index.html',
-        'Controladores Industriales(PLC)':'modulos/PLC/index.html', // //
-        'Desglose de Problema':'modulos/PLC/index.html',
-        'Implementación':'modulos/PLC/index.html',
-        'SCADA':'modulos/SCADA/index.html', // //
-        'Desarrollo HMI':'modulos/SCADA/index.html',
-        'Incorporación HMI':'modulos/SCADA/index.html',
-        'Productos':'productos/index.html', //
-        'Scooter':'productos/index.html',
-        'Cargo E- Bike':'productos/index.html',
-        'Mountain E-Bike':'productos/index.html',
-        'Nosotros': 'Nosotros/index.html',
+    const urlMap = {
+        'index.html':'index.html',
+        './procesoDeAprendizaje/index.html':'procesoDeAprendizaje/index.html',
+        './productoDeIngenieria/index.html':'productoDeIngenieria/index.html', //
+        './modulos/index.html':'modulos/index.html', //
+        './modulos/gestion/index.html':'modulos/gestion/index.html', // //
+        './modulos/industria4/index.html':'modulos/industria4/index.html', // //
+        './modulos/planeacion/index.html':'modulos/planeacion/index.html', // //
+        './modulos/evaluacionEconomica/index.html':'modulos/evaluacionEconomica/index.html', // //
+        './modulos/celdaManufactura/index.html':'modulos/celdaManufactura/index.html', // //
+        './modulos/digitalFactory/index.html':'modulos/digitalFactory/index.html', // //
+        './modulos/PLC/index.html':'modulos/PLC/index.html', // //
+        './modulos/SCADA/index.html':'modulos/SCADA/index.html', // //
+        './productos/index.html':'productos/index.html', //
+        './Nosotros/index.html': 'Nosotros/index.html',
+        '#': 'index.html'
     };
+    const urlMapFooter = {
+        './Nosotros/index.html#johan' : 'Nosotros/index.html#johan',
+        './Nosotros/index.html#david' : 'Nosotros/index.html#david',
+        './Nosotros/index.html#andres': 'Nosotros/index.html#andres',
+        './Nosotros/index.html#luis' : 'Nosotros/index.html#luis',
+        '#': 'index.html',
+        'index.html' : 'index.html'
+    }
     const styleCSS = new ScriptTag('link', '', `href;${path}css/style.css`, 'rel;stylesheet preload prefetch', 'as;style', 'async;true');
     const favicon = new ScriptTag('link','',`href;${path}img/simbolos/logoWhite.svg`,'type;image/svg+xml','async;true','rel;icon');
     const footerCSS = new ScriptTag('link','',`href;${path}css/footer.css`,'rel;stylesheet preload prefetch','as;style','async;true');
-    const scriptJS = new ScriptTag('script', null , `src;${path}js/menu.js`, 'as;script', 'async;true'); //se pasa null para que no se añada automaticamente el script
+    const menuJS = new ScriptTag('script', null , `src;${path}js/menu.js`, 'as;script', 'async;true'); //se pasa null para que no se añada automaticamente el script
+    const overlayMenu = new Element(document.createElement('div')); //overlay es un elemento que no esta dentro del nav del menu pero es importante para su funcionamiento
+    overlayMenu.setAttributes('class;overlay');
 
     const menu = new WebComponent(
-        path, document.createElement('nav'), hrefMap, 'navbar sticky', 'menu.html', '.menu-item,.logo', 'first',
-        { onload: () => { scriptJS.addToElement(); } } // esta función carga el script menu.js despues de que se cargo el webcomponent
+        path, document.createElement('nav'), urlMap, 'navbar sticky', 'menu.html', '.menu-item,.logo', 'first',
+        {
+            onload: () => {
+                overlayMenu.addToElement();//se carga overlay al body
+                menuJS.addToElement();// esta función carga el script menu.js despues de que se cargo el webcomponent
+            }
+        } 
     );
-    const overlayMenu = new WebComponent(path, document.createElement('div'), undefined, 'overlay', undefined, undefined); //CREO QUE ESTO SE PUEDE HACER CON ELEMENT
-    const footer = new WebComponent(path, document.createElement('footer'), hrefMap, 'footer', 'footer.html', 'footer-item');
-
+    const footer = new WebComponent(path, document.createElement('footer'), urlMapFooter, 'footer', 'footer.html', 'footer-item');
     //Usar esto para acceder a las rutas en vez de data-path en cada pagina
     const currentURL = window.location.href;
     console.log(currentURL);
